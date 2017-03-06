ecf3ac562172b42774f781c88f999798daa42b11
ahalf <adamhalfaker@gmail.com>
2017-03-06 18:12:20 -0500

    updated company reg form tested on localhost

--- php/Company_Registration.php
+++ php/Company_Registration.php
@@ -1,22 +1,23 @@
 <?php
-$servername = "dbsrv2.cs.fsu.edu";
-$username = "syed";
-$password = "wpKe*A95XT";
-$database = "halfakerdb";
+$servername = "dbsrv2.cs.fsu.edu"; 
+$username = "syed"; 
+$password = "wpKe*A95XT"; 
+$database = "halfakerdb"; 
 $c = new mysqli($servername, $username, $password, $database);
+
 if($c->connect_error)
 {
-	die("Connection failed " . $c->connect_error);
+    die("Connection failed " . $c->connect_error);
 }
 else
 {
-	echo ("Connected");
+    echo ("Connected");
 }
 
 // Get Company Info
 
 $company_email = $_POST["Email"];
-$company_name = $_POST[""]
+$company_name = $_POST["Uname"];
 $company_pass = $_POST["pass"];
 $company_confirm_pass = $_POST["cpass"];
 $company_phone = $_POST["phone"];
@@ -29,22 +30,26 @@ if($company_pass != $company_confirm_pass)
     echo("ERROR: Passwords do not match\n");
 }
 
-$rows = $c->query("SELECT email FROM Company WHERE $company_email = email");
+$rows = $c->query("SELECT COUNT(email) FROM Company WHERE $company_email = email");
 
-if($rows->num_rows > 0)
+//Make sure Company doesn't already exist
+
+if($rows > 0)
 {
-	echo("ERROR: Company already registered\n");
+    echo("ERROR: Company already registered\n");
 }
+
 else
 {
+
     if($c->query("INSERT INTO Company(password, name, email, phone)
-        VAULES($company_pass, $company_name, $company_email, $company_phone)"))
+        VALUES ('$company_pass', '$company_name', '$company_email', '$company_phone')"))
     {
-    	echo "Successfull entered ". $company_name . " into DB!\n";
+        echo "Successfull entered ". $company_name . " into DB!\n";
     }
     else
     {
-    	echo "Failed to insert ". $company_name . " into DB\n";
+        echo "Failed to insert ". $company_name . " into DB\n";
     }
 }
 
