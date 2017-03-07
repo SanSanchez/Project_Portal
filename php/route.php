37e346c12a5998a2559f01f2851f3f098a2c8146
ahalf <adamhalfaker@gmail.com>
2017-03-07 08:44:41 -0500

    On successful login, direct user to page, fill in user info

--- php/route.php
+++ php/route.php
@@ -1,4 +1,5 @@
 <?php
+	session_start();
 	/*$servername = "dbsrv2.cs.fsu.edu"; 
 	$username = "syed"; 
 	$password = "wpKe*A95XT"; 
@@ -24,20 +25,35 @@
 
 		if($user_type == "student")
 		{
-			echo "Here";
-			$user_exists = $c->query("SELECT COUNT(email) FROM Student WHERE '$email' = email AND '$password' = password");
+			$user_exists = $c->query("SELECT *, COUNT(*) AS total FROM Student WHERE '$email' = email AND '$password' = password");
+			
+				$data = $user_exists->fetch_array(MYSQLI_ASSOC);
+			
 
-			if($user_exists == 1)
+			if($data["total"] == 1)
 			{
-
+				$_SESSIONS['result'] = $data;
 				header("Location: ../html/Student_Profile.html");
 	    		exit;
 			}
+			echo "Username or Password incorrect";
+		}
+		else if($user_type == "business")
+		{
+			$user_exists = $c->query("SELECT *, COUNT(*) AS total FROM Company WHERE '$email' = email AND '$password' = password");
+			
+				$data = $user_exists->fetch_array(MYSQLI_ASSOC);
+			
+
+			if($data["total"] == 1)
+			{
+				$_SESSION['result'] = $data;
+				header("Location: ../php/Company_Profile.php");
+	    		exit;
+			}
+			echo "Username or Password incorrect";
 		}
 	}
-	
-		
-	
-	
+
 
 ?>
