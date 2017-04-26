37cc517d78bd785be417009071380d3824783ac9
SanSanchez <SanSan0037@gmail.com>
2017-04-26 05:03:38 -0400

    Project Creation Commit

--- html/project_creation.php
+++ html/project_creation.php
@@ -1,15 +1,12 @@
 <?php
 session_start();
-
 $username = 'root';
 $password = '';
 $dbname = 'ProjectPortal';
 $host = 'localhost';
 $conn = mysql_connect($host, $username, $password);
  mysql_select_db($dbname, $conn);
-
  $id = $_GET['id'];
-
  ?>
 
 <!DOCTYPE html>
@@ -26,23 +23,17 @@ $conn = mysql_connect($host, $username, $password);
             <div id="form-div">
                 <?php echo "<form id=\"form1\" action=\"../php/project_creation.php?id=$id\" method=\"post\">"?>
 
-                    <p class="ProjName">
                         <label class="Labels" for="ProjName">Project Name</label>
                         <input name="ProjName" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" id="ProjName" />
-                    </p>
 
-                     <p class="Description">
                          <label class="Labels" for="Description">Project Description</label>
                         <textarea name="Description" class="validate[required,length[6,300]] feedback-input" id="Description"></textarea>
-                    </p>
 
                         <div class="nativeDatePicker">
-                            <label for="date">Enter Date</label>
+                            <label class="Labels" for="date">Enter Date</label>
                             <input type="date" id="date" name="date">
                             <span class="validity"></span>
                         </div>
-                      <!--  <p class="fallbackLabel">Enter Project Start Date:</p>
-                        <div class="fallbackDatePicker"> -->
 
                     <div class="submit">
                         <input type="submit" value="Submit" id="SubmitButton"/>
