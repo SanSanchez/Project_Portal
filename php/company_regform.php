cccc9d9299a96d8351656ca8de98e22cc0194296
lexdupper <alexjdupper@gmail.com>
2017-04-25 22:34:22 -0400

    Updates

---
+++ php/company_regform.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+$username = 'root';
+$password = '';
+$dbname = 'ProjectPortal';
+$host = 'localhost';
+$conn = mysql_connect($host, $username, $password);
+ mysql_select_db($dbname, $conn);
+
+
+
+if($conn == false)
+{
+	echo "Connection failed" . $c->connect_error;
+}
+
+$email = $_POST["company_email"];
+$name = $_POST["company_name"];
+$pass = $_POST["company_pass"];
+$confirm = $_POST["cpass"];
+$website = $_POST["website"];
+$phone = $_POST["phone"];
+$mission = $_POST["mission"];
+
+$sql = "SELECT COUNT(email) FROM Company WHERE '$email' = email";
+$result = mysql_query($sql);
+$row = mysql_fetch_row($result);
+
+//confirm the passwords
+if(strcmp($pass,$confirm) !== 0)
+{
+	echo "ERROR: Passwords do not match\n";
+}
+
+//prevents duplicate users
+elseif(count($row) > 1)
+{
+	echo "ERROR: This email has already been used\n";
+}
+
+else
+{
+	$sql = "INSERT INTO Company(password, name, email, phone, website,  mission, project_name, project_descripion) VALUES ('$pass', '$name','$email','$phone', '$website', '$mission', '$project_name', '$project_descripion')";
+	$result = mysql_query($sql);
+	if(!$result)
+	{
+		echo "here";
+		exit;
+	}
+
+		$query = "SELECT id FROM Student WHERE '$email' = email";
+
+		$result = mysql_query($query);
+		$row = mysql_fetch_row($result);
+		$_SESSION['id'] = $row[0];
+		echo $row[0];
+		header("Location: ../../html/Student_Prof.php");
+
+
+}
+
+?>
