7831f2c5f3d237e6a232214d00b2ae4959285d0f
santy <SanSan0037@gmail.com>
2018-03-13 00:17:19 -0400

    starting to redo studentsignup.html

---
+++ html/header.php
@@ -0,0 +1,10 @@
+<html lang="en">
+
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: santy
+ * Date: 3/3/2018
+ * Time: 2:57 PM
+ */
---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/test.css">
+    </head>
+    <body><?php
+
+    echo "<h1>Hello World!</h1>";
+    ?>
+    </body>
+</html>
